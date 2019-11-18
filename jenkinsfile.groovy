b1a72259c3d0e9b79c08e35472adee0dbeed9b9d
310158964 <310158964@NLYBSTQVP4NB88V>
2019-11-18 15:01:52 +0100

    adding new jenkins file

---
+++ jenkinsfile.groovy
@@ -0,0 +1,60 @@
+pipeline {
+    agent none
+    options {
+        buildDiscarder(logRotator(daysToKeepStr: '18'))
+    }
+    parameters {
+        string defaultValue: 'Head', description: 'Capacity Passed Revision', name: 'Revision', trim: false
+    }
+    environment {
+        FORTIFY_URL = "https://fortify.philips.com/ssc"
+        FORTIFY_REPORT_LOCATION = "C:\\Views\\Build_Bmainline\\Output\\Publish\\Fortify\\"
+    }
+    stages{
+        stage('Checkout/update source code') {
+            agent {label 'fortifybuildserver' }
+            options {
+                timeout(time: 2, unit: 'HOURS')
+                timestamps()
+            }
+            steps  {
+                checkout changelog: false, poll: false, scm: [$class: 'SubversionSCM', additionalCredentials: [],
+                 excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false,
+                  ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: '5f139fb7-9e46-44a7-ac1e-fd010ec54cc4',
+                   depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: 'https://svn-01.ta.philips.com/svn/icap-platform/trunk@${params.Revision}']],
+                    quietOperation: false, workspaceUpdater: [$class: 'UpdateUpdater']]
+            }
+        }
+        // stage('Upload Fortify Reports') {
+        //     agent {label 'fortifybuildserver'}
+        //     options {
+        //         timeout(time: 1, unit: 'HOURS')
+        //         timestamps()
+        //     }
+        //     steps{
+        //         script {
+        //             def codebaseNames = ["AII", "AIP", "AppDev", "Build", "Common", "ManagedServices", "SII", "System"]                    
+        //             codebaseNames.each { 
+        //                 codebaseName -> UploadCodebaseFortifyReport(codebaseName)
+        //                 }
+        //             }
+                    
+        //         }
+        //     }   
+        // }
+    }
+
+def UploadCodebaseFortifyReport(String codebaseName) {
+    script{
+        withCredentials([string(credentialsId: "FortifyUploadToken-${codebaseName}", variable: 'TOKEN')]) {
+            def reportFile = "${FORTIFY_REPORT_LOCATION}${codebaseName}Scan.fpr"
+            def codebaseVersion = "HSDP-CP-${codebaseName}"
+            echo "Uploading: ${reportFile} to ${codebaseVersion}"
+            RunBatch("fortifyclient -url ${FORTIFY_URL} -authtoken ${TOKEN} uploadFPR -file ${reportFile} -project HSDP-CP -version ${codebaseVersion}")
+        }
+    }
+}
+
+def RunBatch(String command) {
+    bat returnStatus: false, script:"${command}"
+}
