39f81311be5f1b5f82c2137d5efdef788cd5ca56
310158964 <310158964@NLYBSTQVP4NB88V>
2019-11-22 13:47:13 +0100

    added few comments from demo

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,5 +1,5 @@
 pipeline {
-    agent none
+    agent {label 'fortifybuildserver' }
     options {
         buildDiscarder(logRotator(daysToKeepStr: '18'))
     }
@@ -12,21 +12,33 @@ pipeline {
     }
     stages{
         stage('Checkout/update source code') {
-            agent {label 'fortifybuildserver' }
             options {
                 timeout(time: 2, unit: 'HOURS')
                 timestamps()
             }
             steps  {
-                checkout changelog: false, poll: false, scm: [$class: 'SubversionSCM', additionalCredentials: [],
-                 excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false,
-                  ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: '5f139fb7-9e46-44a7-ac1e-fd010ec54cc4',
-                   depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: "https://svn-01.ta.philips.com/svn/icap-platform/trunk@${params.Revision}"]],
-                    quietOperation: false, workspaceUpdater: [$class: 'UpdateUpdater']]
+                checkout changelog: false, 
+                poll: false, 
+                scm: [$class: 'SubversionSCM',
+                additionalCredentials: [],
+                excludedCommitMessages: '',
+                excludedRegions: '',
+                excludedRevprop: '',
+                excludedUsers: '',
+                filterChangelog: false,
+                ignoreDirPropChanges: false,
+                includedRegions: '',
+                locations: [[cancelProcessOnExternalsFail: true,
+                credentialsId: '5f139fb7-9e46-44a7-ac1e-fd010ec54cc4',
+                depthOption: 'infinity',
+                ignoreExternalsOption: false,
+                local: '.',
+                remote: 'https://svn-01.ta.philips.com/svn/icap-platform/trunk']],
+                quietOperation: true,
+                workspaceUpdater: [$class: 'CheckoutUpdater']]
             }
         }
         stage ('IPF Build') {
-            agent {label 'fortifybuildserver' }
             options {
                 timeout(time: 2, unit: 'HOURS')
                 timestamps()
@@ -38,13 +50,13 @@ pipeline {
             }
         }
         stage ('Fortify Build and Scan') {
-            agent {label 'fortifybuildserver' }
             options {
                 timeout(time: 5, unit: 'HOURS')
                 timestamps()
             }
             steps {
                 script {
+                    //TODO: replace below call with dedicated build.bat option
                     RunBatch("""call %WORKSPACE%\\build_\\build\\vsvars.cmd
                     call msbuild %WORKSPACE%\\build.targets /t:_check /p:EnableFortifyCheck=true /p:DisableCodeAnalysis=true /p:UseSharedArtifactoryAuthentication=true /p:CheckLicenseBuild=false /p:DeploymentConfiguration=server
                     """)
@@ -52,7 +64,6 @@ pipeline {
             }
         }
         stage('Upload Fortify Reports') {
-            agent {label 'fortifybuildserver'}
             options {
                 timeout(time: 1, unit: 'HOURS')
                 timestamps()
@@ -76,6 +87,7 @@ def UploadCodebaseFortifyReport(String codebaseName) {
             def reportFile = "${WORKSPACE}\\${codebaseName}\\${FORTIFY_REPORT_LOCATION}${codebaseName}Scan.fpr"
             def codebaseVersion = "HSDP-CP-${codebaseName}"
             echo "Uploading: ${reportFile} to ${codebaseVersion}"
+            //TODO: Check if revision can be added as a param
             RunBatch("fortifyclient -url ${FORTIFY_URL} -authtoken ${TOKEN} uploadFPR -file ${reportFile} -project HSDP-CP -version ${codebaseVersion}")
         }
     }
